3dcb4cd634233f1c9040c5794f3edda8d34667df
mnorouzi <milad.norouzi@ionos.com>
2025-08-03 20:00:23 +0200

    add ansible project

---
+++ milad-norouzi/ansible-project/build_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "*************************************************"
+echo "Starting build process..."
+echo `date +%Y-%m-%d\ %H:%M:%S` > /tmp/ansible_project1.txt
+sleep 10
+echo  `date +%Y-%m-%d\ %H:%M:%S` >> /tmp/ansible_project1.txt
+echo "Build process completed."
+echo "*************************************************"
+
